b3643928ae3591ccf7c2ccbb348b3dedf90ead9c
Olga Kutueva <kutueva.olga@gmail.com>
2023-06-09 15:10:05 +0300

    Задача на возвращение кода ошибки

---
+++ Seminar_1/Main.java
@@ -0,0 +1,24 @@
+package Seminar_1;
+
+// реализуйте метод, принимающий в качестве аргумента целочисленный массив
+// если длина массива меньше некоторого заданного минимума, метод возв. -1, иначе - длину массива
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int []{1, 2, 3, 4};
+        int a = ex0(array);
+        if (a == -1)
+            System.out.println("Return -1");
+        else {
+
+        };
+    }
+
+    static int ex0(int[] array) {
+        int minSize = 5;
+        if (array.length < minSize)
+            return -1;
+        else
+            return array.length;
+    }
+}
