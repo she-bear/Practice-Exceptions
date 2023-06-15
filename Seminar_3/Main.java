6d0d3f6ef26fa3cdb5f2bbb7eb69bc5fe8d1d4df
Olga Kutueva <kutueva.olga@gmail.com>
2023-06-15 12:23:45 +0300

    Итоговое задание: ввод исходных данных

--- Seminar_3/Main.java
+++ Seminar_3/Main.java
@@ -1,8 +1,31 @@
 package Seminar_3;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         
-        
+        try {
+            System.out.println(Arrays.toString(getUserData()));
+        } catch (RuntimeException exc) {
+            System.out.println(exc.getMessage());
+        }
+    }
+
+    // ввод исходных данных
+    public static String[] getUserData() {
+        Scanner iScanner = new Scanner(System.in, "cp866");
+        System.out.print("Enter data separated by spaces: ");
+        String raw_data = iScanner.nextLine();
+
+        String[] split_data;
+        split_data = raw_data.split(" ");
+
+        if (split_data.length != 6){
+            throw new RuntimeException("Insufficient amount of data!");
+        }
+
+        return split_data;
     }
 }
